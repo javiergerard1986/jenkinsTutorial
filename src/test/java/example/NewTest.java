157cbb660d60860458e45c5e2f63e4caf733bd5e
Javier Gerard <javier.gerard@autodesk.com>
2017-08-09 00:53:30 -0300

    Adding comment to Change B branch

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -15,6 +15,7 @@ public class NewTest {
 	@Test				
 	public void testEasy() {	
 			
+		//Comment to delete
 		driver.get("http://demo.guru99.com/selenium/guru99home/");  
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
